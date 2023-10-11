f030f77ea2beb83eff064e091840606e56124682
pedrogrande <pete@peterargent.com>
2023-10-11 22:37:38 +1100

    initial commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+(() => {
+	'use strict'
+	const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	tooltipTriggerList.forEach(tooltipTriggerEl => {
+		new bootstrap.Tooltip(tooltipTriggerEl)
+	})
+	document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+		document.querySelector('.offcanvas-collapse').classList.toggle('open')
+	})
+})()
